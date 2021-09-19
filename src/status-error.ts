02d83f8c5658718f4fc3b35905ca9cbf22dd2b05
mei23 <m@m544.net>
2021-09-19 22:11:08 +0900

    fix status-error

--- src/status-error.ts
+++ src/status-error.ts
@@ -1,10 +1,10 @@
 
 export class StatusError extends Error {
-	public statusCode?: number;
+	public statusCode: number;
 	public statusMessage?: string;
 	public isPermanentError: boolean;
 
-	constructor(message: string, statusCode?: number, statusMessage?: string) {
+	constructor(message: string, statusCode: number, statusMessage?: string) {
 		super(message);
 		this.name = 'StatusError';
 		this.statusCode = statusCode;
